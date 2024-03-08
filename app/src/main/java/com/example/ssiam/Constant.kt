813d343e8d94c0b26e4ff35e8f959f2da10fe0d2
DucNHH <ducnhh165@gmail.com>
2024-03-08 21:11:31 +0700

    Fix Github access token issue

--- app/src/main/java/com/example/ssiam/Constant.kt
+++ app/src/main/java/com/example/ssiam/Constant.kt
@@ -2,10 +2,10 @@ package com.example.ssiam
 
 object Constant {
     const val VALUE_URL = "https://www.ssi.com.vn/khach-hang-ca-nhan/hieu-qua-dau-tu-cua-quy-sca"
-    const val UPDATE_URL = "https://api.github.com/repos/DucNHH/demo1/releases/latest"
+    const val TOKEN_URL = "https://api.thingspeak.com/channels/2454460/feeds/last.json?api_key=WWMIUD0U48OX08R3"
+    const val UPDATE_URL = "https://api.github.com/repos/DucNHH/SSIAM/releases/latest"
     const val PREF_NAME = "com.example.ssiam"
     const val PREF_MONEY = "money"
-    const val AUTHORIZATION_TOKEN = "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"
     const val BACKGROUND_DELAY = 6 * 60 * 60 * 1000L
     const val FOREGROUND_DELAY = 5 * 1000L
     const val CCQ_AMOUNT = 14.63
